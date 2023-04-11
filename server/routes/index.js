41100fd2d30b25cc493285845fdfc7a6b2bc84be
blumkvist1 <101320457+blumkvist1@users.noreply.github.com>
2023-04-11 13:37:31 +0300

    add check role to analitics

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,11 @@ const router = new Router();
 const courseRouter = require("./courseRouter");
 const userRouter = require("./userRouter");
 const analiticsRouter = require("./analiticsRouter");
+const checkRoleMiddleware = require("../middleware/checkRoleMiddleware");
 
 router.use("/", courseRouter);
 router.use("/user", userRouter);
 router.use("/", courseRouter);
-router.use("/analitics", analiticsRouter);
+router.use("/analitics", checkRoleMiddleware("ADMIN"), analiticsRouter);
 
 module.exports = router;
